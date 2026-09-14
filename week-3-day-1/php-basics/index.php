1661e8172c9b3571ff86ffb92a6e1d1192a9d456
Praveen Kumar <praveenkumarks0405@gmail.com>
2026-09-14 19:22:30 +0530

    feat: php-fundamentals week-3-day-1 complete

---
+++ week-3-day-1/php-basics/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Form</title>
+</head>
+<body>
+
+<h2>User Form</h2>
+
+<form action="process.php" method="POST">
+    <input type="text" name="name" placeholder="Enter Name">
+    <input type="email" name="email" placeholder="Enter Email">
+    <button type="submit">Submit</button>
+</form>
+
+</body>
+</html>
